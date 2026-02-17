4cb97643e19537b8565c19ecdd510344cbe2fb64
unaizhashim6-code <unaizhashim6@gmail.com>
2026-02-17 20:20:10 +0530

    Remove date/time functions and add load message
    Removed date and time functions and added a console log.

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
-// JavaScript functionality for the landing page
-
-// Function to get the current date and time
-function getCurrentDateTime() {
-    const now = new Date();
-    return now.toUTCString(); // Returning in UTC
-}
-
-// Function to display current date and time on the webpage
-function displayDateTime() {
-    const dateTimeContainer = document.getElementById('datetime');
-    if (dateTimeContainer) {
-        dateTimeContainer.innerHTML = getCurrentDateTime();
-    }
-}
-
-// Call displayDateTime on page load
-window.onload = displayDateTime;
+console.log("AEROSTEP Website Loaded Successfully!");
